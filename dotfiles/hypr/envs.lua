39af46c0df16af7ff2f57785137e5524b9dbb4ec
Will06 <wilberhernan06@gmail.com>
2026-06-28 08:49:21 -0500

    switch to Oxygen Neon White cursor

--- dotfiles/hypr/envs.lua
+++ dotfiles/hypr/envs.lua
@@ -5,7 +5,7 @@ hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
 -- Cursor theme
-hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_THEME", "oxy-neon-white")
 hl.env("XCURSOR_SIZE", "20")
-hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_THEME", "oxy-neon-white")
 hl.env("HYPRCURSOR_SIZE", "20")
