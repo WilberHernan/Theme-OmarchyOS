2e020f995f520bcec55f3467db44cad6cfad2de3
Will06 <wilberhernan06@gmail.com>
2026-06-28 09:18:02 -0500

    fix: neofetch --off to suppress CachyOS logo
    neofetch (no flags) defaults to distro logo.
    --off shows specs only, no image/ascii.

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,6 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting - neofetch specs only
+# overwrite greeting - neofetch specs only (no logo)
 function fish_greeting
-    neofetch
+    neofetch --off
 end
