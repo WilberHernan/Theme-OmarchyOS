b7325f0f9d99b2f0769d07fec18961892808f868
Will06 <wilberhernan06@gmail.com>
2026-09-05 13:18:23 -0500

    style(fish): neutral comment for greeting override

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,5 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# Terminal limpia: anular el greeting de CachyOS (corre )
+# Terminal limpia: anular el greeting de CachyOS
 function fish_greeting
 end
